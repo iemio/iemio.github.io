c148d7b2ebda8072c840684e1128d7e4e6298e1b
iemio <deeplearner4u@gmail.com>
2025-03-25 03:43:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div style={{ textAlign: "center", padding: "50px" }}>
-            <h1>🚀 Welcome to My Next.js App Router Site</h1>
+            <h1>🚀 Welcome to My Git Site</h1>
             <p>
                 This site is deployed on <b>GitHub Pages</b> using the App
                 Router.
