c1165afd16c57329d887bc4a734995e84b172faa
iemio <deeplearner4u@gmail.com>
2025-03-25 15:33:54 +0530

    commit for iemio.github.io

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>🚀 Welcome to blog page</h1>
+        </div>
+    );
+}
