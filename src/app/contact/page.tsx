9f29264de8a30ecf0b503462b81c3be5017b5953
iemio <deeplearner4u@gmail.com>
2025-03-31 17:51:55 +0530

    feat: add

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+import "@/styles/contact.css";
+
+import { FaInstagram, FaYoutube, FaLinkedinIn } from "react-icons/fa";
+
+export default function Projects() {
+    return (
+        <section className="contact section" id="contact">
+            <div className="contact__container grid">
+                <div className="contact__data">
+                    <h2 className="section__title-2">
+                        <span>Contact Me.</span>
+                    </h2>
+
+                    <p className="contact__description-1">
+                        I will read all emails. Send me any message you want and
+                        I'll get back to you.
+                    </p>
+
+                    <p className="contact__description-2">
+                        I need your <b>Name</b> and <b>Email Address</b>, but
+                        you won't receive anything other than your reply.
+                    </p>
+
+                    <div className="geometric-box"></div>
+                </div>
+
+                <div className="contact__mail">
+                    <h2 className="contact__title">Send Me A Message</h2>
+
+                    <form action="" className="contact__form" id="contact-form">
+                        <div className="contact__group">
+                            <div className="contact__box">
+                                <input
+                                    type="text"
+                                    name="user_name"
+                                    className="contact__input"
+                                    id="name"
+                                    required
+                                    placeholder="First name"
+                                />
+                                <label
+                                    htmlFor="name"
+                                    className="contact__label"
+                                >
+                                    First Name
+                                </label>
+                            </div>
+
+                            <div className="contact__box">
+                                <input
+                                    type="email"
+                                    name="user_email"
+                                    className="contact__input"
+                                    id="email"
+                                    required
+                                    placeholder="Email Address"
+                                />
+                                <label
+                                    htmlFor="email"
+                                    className="contact__label"
+                                >
+                                    Email Address
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="contact__box">
+                            <input
+                                type="email"
+                                name="user_subject"
+                                className="contact__input"
+                                id="subject"
+                                required
+                                placeholder="Subject"
+                            />
+                            <label htmlFor="subject" className="contact__label">
+                                Subject
+                            </label>
+                        </div>
+
+                        <div className="contact__box contact__area">
+                            <textarea
+                                name="user_message"
+                                id="message"
+                                className="contact__input"
+                                required
+                                placeholder="Message"
+                            ></textarea>
+                            <label htmlFor="message" className="contact__label">
+                                Message
+                            </label>
+                        </div>
+
+                        <p
+                            className="contact__message"
+                            id="contact-message"
+                        ></p>
+
+                        <button
+                            type="submit"
+                            className="contact__button button"
+                        >
+                            <i className="ri-send-plane-line"></i> Send Message
+                        </button>
+                    </form>
+                </div>
+
+                <div className="contact__social">
+                    <img
+                        src="assets/img/curved-arrow.svg"
+                        alt=""
+                        className="contact__social-arrow"
+                    />
+
+                    <div className="contact__social-data">
+                        <div>
+                            <p className="contact__social-description-1">
+                                Does not send emails
+                            </p>
+
+                            <p className="contact__social-description-2">
+                                Write me on my social networks
+                            </p>
+                        </div>
+
+                        <div className="contact__social-links">
+                            <a
+                                href="https://www.youtube.com/"
+                                target="_blank"
+                                className="contact__social-link"
+                            >
+                                {/* <i className="ri-facebook-circle-line"></i> */}
+                                <FaYoutube />
+                            </a>
+
+                            <a
+                                href="https://www.instagram.com/"
+                                target="_blank"
+                                className="contact__social-link"
+                            >
+                                {/* <i className="ri-instagram-line"></i> */}
+                                <FaInstagram />
+                            </a>
+
+                            <a
+                                href="https://www.linkedin.com/"
+                                target="_blank"
+                                className="contact__social-link"
+                            >
+                                <FaLinkedinIn />
+                                {/* <i className="ri-linkedin-box-line"></i> */}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,78 @@
+"use client"; // Required for App Router
+
+import { FaLinkedinIn, FaGithub, FaCodepen } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa";
+import { HiOutlineChevronDown } from "react-icons/hi";
+import "@/styles/home.css";
+
 export default function Home() {
     return (
-        <div style={{ textAlign: "center", padding: "50px" }}>
-            <h1>🚀 Welcome to My Git Site</h1>
-            <p>
-                This site is deployed on <b>GitHub Pages</b> using the App
-                Router.
-            </p>
-        </div>
+        <section className="home section" id="home">
+            <div className="home__container container grid">
+                <h1 className="home__name">Keshav</h1>
+
+                <div className="home__perfil">
+                    <div className="home__image">
+                        <img
+                            src="./user.jpeg"
+                            alt="image"
+                            className="home__img"
+                        />
+                        <div className="home__shadow"></div>
+
+                        <img src="" alt="" className="home__arrow" />
+                        <img src="" alt="" className="home__line" />
+
+                        <div className="geometric-box"></div>
+                    </div>
+
+                    <div className="home__social">
+                        <a
+                            href="https://www.linkedin.com/"
+                            target="_blank"
+                            className="home__social-link"
+                        >
+                            {/* <i className="ri-linkedin-box-fill"></i> */}
+                            <FaLinkedinIn />
+                        </a>
+
+                        <a
+                            href="https://github.com/"
+                            target="_blank"
+                            className="home__social-link"
+                        >
+                            {/* <i className="ri-github-fill"></i> */}
+                            <FaGithub />
+                        </a>
+
+                        <a
+                            href="https://codepen.io"
+                            target="_blank"
+                            className="home__social-link"
+                        >
+                            {/* <i className="ri-codepen-fill"></i> */}
+                            <FaCodepen />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="home__info">
+                    <p className="home__description">
+                        <b>Active learner</b>, with knowledge in web
+                        development, app development and machine learning. I
+                        offer the best projects resulting in quality work.
+                    </p>
+
+                    <a href="#about" className="home__scroll">
+                        <div className="home__scroll-box">
+                            <HiOutlineChevronDown className="scroll-arrow" />
+                            {/* <i className="ri-arrow-down-s-line"></i> */}
+                        </div>
+
+                        <span className="home__scroll-text">Scroll Down</span>
+                    </a>
+                </div>
+            </div>
+        </section>
     );
 }
