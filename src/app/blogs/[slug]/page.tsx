e3bb98542e522262280fbed6caa997ea4f7786d2
iemio <deeplearner4u@gmail.com>
2025-04-09 03:08:05 +0530

    fix

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,12 +1,13 @@
 // app/blog/[slug]/page.tsx
-
 import { getBlogBySlug, getAllBlogSlug } from "../fetchers";
 
 // We'll inline the type for clarity, ensuring it matches Next.js expectations.
 export async function generateStaticParams() {
-    return getAllBlogSlug();
+    const slugs = getAllBlogSlug();
+    console.log("slugs", slugs);
+    return slugs;
 }
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function BlogPage({
     params,
     searchParams,
