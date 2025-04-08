932bbcce5cce1bdf31dc8f84c4b6f439f682300c
iemio <deeplearner4u@gmail.com>
2025-04-09 02:44:36 +0530

    fix

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,6 +1,5 @@
 // app/blog/[slug]/page.tsx
 
-import { notFound } from "next/navigation";
 import { getBlogBySlug, getAllBlogSlug } from "../fetchers";
 
 // We'll inline the type for clarity, ensuring it matches Next.js expectations.
