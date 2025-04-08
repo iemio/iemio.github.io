54af6813f0ba821e41c0c948cf283ec8614dd555
iemio <deeplearner4u@gmail.com>
2025-04-08 18:37:29 +0530

    fix: bug

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -11,13 +11,17 @@ type BlogPageProps = {
     };
 };
 
-export default async function BlogPage({ params }: BlogPageProps) {
+export default async function BlogPage({
+    params,
+}: {
+    params: { slug: string };
+}) {
     const { slug } = params;
 
     try {
         const blog = await getBlogBySlug(slug);
         return (
-            <main className="prose prose-gray mt-24 pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
+            <main className="prose dark:prose-invert mt-24 pb-20">
                 <article>{blog.content}</article>
             </main>
         );
