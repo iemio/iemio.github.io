8685e0d52a1f785ebba7088a9504753638fbac6d
iemio <deeplearner4u@gmail.com>
2025-04-09 05:16:54 +0530

    fixed

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,13 +1,15 @@
 // app/blog/[slug]/page.tsx
 import { getAllBlogSlug, getBlogBySlug } from "../fetchers";
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
+export async function generateStaticParams() {
     const slugs = await getAllBlogSlug();
     console.log("slugs", slugs);
     return slugs;
 }
 
+//https://nextjs.org/docs/app/building-your-application/upgrading/version-15#async-request-apis-breaking-change
 type Params = Promise<{ slug: string }>;
+
 export default async function BlogPage({ params }: { params: Params }) {
     const { slug } = await params;
     const blog = await getBlogBySlug(slug);
@@ -17,12 +19,3 @@ export default async function BlogPage({ params }: { params: Params }) {
         </main>
     );
 }
-
-// export default async function BlogPage() {
-//     const blog = await getBlogBySlug("blog1");
-//     return (
-//         <main className="prose">
-//             <article>{blog.content}</article>
-//         </main>
-//     );
-// }
