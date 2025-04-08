3be8cc33f28aa4e6c0c529c28866ba718fee40e9
iemio <deeplearner4u@gmail.com>
2025-04-09 04:46:22 +0530

    chk

--- src/app/project/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-    return <div>here comes project with some param</div>;
-};
-
-export default page;
--- src/app/project/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-    return <div>here comes all projects.</div>;
-};
-
-export default page;
