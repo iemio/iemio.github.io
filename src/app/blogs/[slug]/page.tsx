c921698fde8080019cb3df59bea26e66c92f3b22
iemio <deeplearner4u@gmail.com>
2025-04-09 05:08:45 +0530

    chk

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -4,33 +4,28 @@ import { getAllBlogSlug, getBlogBySlug } from "../fetchers";
 export async function generateStaticParams() {
     const slugs = await getAllBlogSlug();
     console.log("slugs", slugs);
-    return slugs;
+    // return slugs;
+    return [{ slug: "blog1" }, { slug: "blog2" }];
 }
-
-// export default async function BlogPage({
-//     params,
-// }: {
-//     params: { slug: string };
-// }) {
-//     // const blog = await getBlogBySlug(params.slug);
-//     return (
-//         <main className="prose">
-//             <article>{"blog.content"}</article>
-//         </main>
-//     );
-// }
 
 export default async function BlogPage({
     params,
 }: {
     params: { slug: string };
 }) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { slug } = await params;
-    const blog = await getBlogBySlug("blog1");
+    const blog = await getBlogBySlug(params.slug);
     return (
         <main className="prose">
             <article>{blog.content}</article>
         </main>
     );
 }
+
+// export default async function BlogPage() {
+//     const blog = await getBlogBySlug("blog1");
+//     return (
+//         <main className="prose">
+//             <article>{blog.content}</article>
+//         </main>
+//     );
+// }
