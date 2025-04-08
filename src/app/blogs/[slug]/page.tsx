2d3105bd5c36076df2f06d5f7c386e3e402a6c34
iemio <deeplearner4u@gmail.com>
2025-04-08 18:32:46 +0530

    fix: bug

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function BlogPage({
 }: {
     params: { slug: string };
 }) {
-    const { slug } = await params;
+    const { slug } = params;
 
     try {
         const blog = await getBlogBySlug(slug);
